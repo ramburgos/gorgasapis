6c97c1a2096b47c3cbf769521ffcbcb96ca1bc1c
Ramon Burgos Dev <ramonburgosgarcia@gmail.com>
2025-06-10 14:17:22 -0500

    dockerizado10062025

--- database.py
+++ database.py
@@ -6,14 +6,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#MYSQL_USER=os.getenv("MYSQL_USER",)
-#MYSQL_PASSWORD=os.getenv("MYSQL_PASSWORD")
-#MYSQL_DB=os.getenv("MYSQL_DB")
-#MYSQL_HOST=os.getenv("MYSQL_HOST")
-#MYSQL_PORT=int(os.getenv("MYSQL_PORT",3306))
-
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://dbuser25:dbslave_sigorgas@sigorgas-databasesigorgas-3n89yi:3306/db_gorgas_crm")
-# DATABASE_URL= f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+MYSQL_USER=os.getenv("MYSQL_USER",)
+MYSQL_PASSWORD=os.getenv("MYSQL_PASSWORD")
+MYSQL_DB=os.getenv("MYSQL_DB")
+MYSQL_HOST=os.getenv("MYSQL_HOST")
+MYSQL_PORT=int(os.getenv("MYSQL_PORT",3306))
+DATABASE_URL= f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
 engine = create_engine(DATABASE_URL)
 Sessionlocal = sessionmaker(bind=engine)
