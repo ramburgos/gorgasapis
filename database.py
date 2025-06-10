f342402260042e0dd9a97fda040eb1948dc0be98
Ramon Burgos Dev <ramonburgosgarcia@gmail.com>
2025-06-10 13:41:29 -0500

    dockerizado10062025

--- database.py
+++ database.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MYSQL_USER=os.getenv("MYSQL_USER")
+MYSQL_USER=os.getenv("MYSQL_USER",)
 MYSQL_PASSWORD=os.getenv("MYSQL_PASSWORD")
 MYSQL_DB=os.getenv("MYSQL_DB")
-MYSQL_HOST=os.getenv("MYSQL_HOST")
+MYSQL_HOST=os.getenv("MYSQL_HOST","sigorgas-databasesigorgas-3n89yi")
 MYSQL_PORT=int(os.getenv("MYSQL_PORT",3306))
 
 DATABASE_URL= f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
