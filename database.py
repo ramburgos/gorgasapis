2c8be3101dcb314ddd67e0d53cf5d4c4c25b1924
Ramon Burgos Dev <ramonburgosgarcia@gmail.com>
2025-07-09 09:29:40 -0500

    second commit

--- database.py
+++ database.py
@@ -5,7 +5,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://dbuser25:dbslave_sigorgas@sigorgas-databasesigorgas-3n89yi:3306/db_gorgas_crm")
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://dbuser25:dbslave_sigorgas@172.31.1.71:3030/db_gorgas_crm")
+#DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://dbuser25:dbslave_sigorgas@sigorgas-databasesigorgas-3n89yi:3306/db_gorgas_crm")
 engine = create_engine(DATABASE_URL)
 Sessionlocal = sessionmaker(bind=engine)
 
